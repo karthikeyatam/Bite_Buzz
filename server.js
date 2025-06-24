e4e288057d31b58e82b9b19744b825a28e9a8dff
karthikeyatam <96367389+karthikeyatam@users.noreply.github.com>
2025-06-24 21:49:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,101 +1,112 @@
-const PORT = process.env.PORT || 4000;
-
-const _0x5af7c2 = _0x3a4e;
-function _0x3a4e(_0x3a1b85, _0x512278) {
-  const _0x4126ab = _0x4126();
+const _0x948281 = _0x4d80;
+function _0x4d80(_0x47d2b2, _0xbfc548) {
+  const _0x10b67c = _0x10b6();
   return (
-    (_0x3a4e = function (_0x56610f, _0x2f1d96) {
-      _0x56610f = _0x56610f - 0x1a0;
-      let _0x2f2da5 = _0x4126ab[_0x56610f];
-      return _0x2f2da5;
+    (_0x4d80 = function (_0x4d8001, _0x5d899a) {
+      _0x4d8001 = _0x4d8001 - 0x16a;
+      let _0x5cef81 = _0x10b67c[_0x4d8001];
+      return _0x5cef81;
     }),
-    _0x3a4e(_0x3a1b85, _0x512278)
+    _0x4d80(_0x47d2b2, _0xbfc548)
   );
 }
-(function (_0x52ad44, _0x2c7f96) {
-  const _0x4c480b = _0x3a4e,
-    _0x57c1cc = _0x52ad44();
+(function (_0x477f34, _0x5aca57) {
+  const _0x5595ac = _0x4d80,
+    _0x42037e = _0x477f34();
   while (!![]) {
     try {
-      const _0x410197 =
-        -parseInt(_0x4c480b(0x1b3)) / 0x1 +
-        -parseInt(_0x4c480b(0x1b6)) / 0x2 +
-        -parseInt(_0x4c480b(0x1b1)) / 0x3 +
-        (parseInt(_0x4c480b(0x1b9)) / 0x4) *
-          (parseInt(_0x4c480b(0x1ae)) / 0x5) +
-        parseInt(_0x4c480b(0x1b8)) / 0x6 +
-        -parseInt(_0x4c480b(0x1b4)) / 0x7 +
-        parseInt(_0x4c480b(0x1a3)) / 0x8;
-      if (_0x410197 === _0x2c7f96) break;
-      else _0x57c1cc["push"](_0x57c1cc["shift"]());
-    } catch (_0x5abf99) {
-      _0x57c1cc["push"](_0x57c1cc["shift"]());
+      const _0x1c421b =
+        -parseInt(_0x5595ac(0x16d)) / 0x1 +
+        (parseInt(_0x5595ac(0x16f)) / 0x2) *
+          (parseInt(_0x5595ac(0x172)) / 0x3) +
+        -parseInt(_0x5595ac(0x173)) / 0x4 +
+        (parseInt(_0x5595ac(0x176)) / 0x5) *
+          (parseInt(_0x5595ac(0x186)) / 0x6) +
+        (-parseInt(_0x5595ac(0x182)) / 0x7) *
+          (-parseInt(_0x5595ac(0x177)) / 0x8) +
+        parseInt(_0x5595ac(0x184)) / 0x9 +
+        (-parseInt(_0x5595ac(0x17a)) / 0xa) *
+          (parseInt(_0x5595ac(0x175)) / 0xb);
+      if (_0x1c421b === _0x5aca57) break;
+      else _0x42037e["push"](_0x42037e["shift"]());
+    } catch (_0x413fea) {
+      _0x42037e["push"](_0x42037e["shift"]());
     }
   }
-})(_0x4126, 0x5ba1f);
-
+})(_0x10b6, 0x899d2);
 const app = require("./app"),
-  connectDatabase = require(_0x5af7c2(0x1ab)),
-  dotenv = require(_0x5af7c2(0x1aa)),
-  cloudinary = require("cloudinary");
-
-process["on"](_0x5af7c2(0x1b2), (_0x25baba) => {
-  console["log"]("Uncaught Exception:", _0x25baba["message"]);
-  process ;
-});
-
-dotenv[_0x5af7c2(0x1a1)]({ path: _0x5af7c2(0x1a5) });
-
-connectDatabase();
-
-cloudinary[_0x5af7c2(0x1a1)]({
-  cloud_name: process["env"]["CLOUDINARY_CLOUD_NAME"],
-  api_key: process["env"][_0x5af7c2(0x1a7)],
-  api_secret: process["env"][_0x5af7c2(0x1ac)],
-});
-
-app["get"]("/", (_0xreq, _0xres) => {
-  _0xres["send"]("API is running");
-});
-
-const server = app[_0x5af7c2(0x1a9)](PORT, () => {
-  console["log"](
-    `Server running on port ${PORT} in ${process["env"][_0x5af7c2(0x1a4)] || "development"} mode`
-  );
-});
-
-process["on"](_0x5af7c2(0x1a2), (_0xerr) => {
-  console["log"]("Unhandled Rejection:", _0xerr["message"]);
-  server[_0x5af7c2(0x1ad)](() => {
-    process ;
+  connectDatabase = require(_0x948281(0x16c)),
+  dotenv = require(_0x948281(0x185)),
+  cloudinary = require("cloudinary"),
+  { setDriver } = require(_0x948281(0x17b));
+process["on"](_0x948281(0x179), (_0x292333) => {
+  const _0x2a3ed7 = _0x948281;
+  console[_0x2a3ed7(0x16e)](_0x2a3ed7(0x183) + _0x292333["stack"]),
+    console[_0x2a3ed7(0x16e)](_0x2a3ed7(0x180)),
+    process[_0x2a3ed7(0x17f)](0x1);
+}),
+  dotenv[_0x948281(0x16a)]({ path: _0x948281(0x171) }),
+  connectDatabase(),
+  cloudinary[_0x948281(0x16a)]({
+    cloud_name: process[_0x948281(0x170)]["CLOUDINARY_CLOUD_NAME"],
+    api_key: process[_0x948281(0x170)][_0x948281(0x17d)],
+    api_secret: process["env"][_0x948281(0x178)],
   });
-});
-
-function _0x4126() {
-  const _0x3c6e59 = [
-    "config",
+function _0x10b6() {
+  const _0x5cdbf7 = [
+    "963008uNFeya",
+    "PORT",
+    "373021GonJyL",
+    "55okMaYG",
+    "44648zwzYyN",
+    "CLOUDINARY_API_SECRET",
+    "uncaughtException",
+    "250LXKfcT",
+    "mongoose",
+    "listen",
+    "CLOUDINARY_API_KEY",
     "close",
     "exit",
-    "uncaughtException",
-    "CLOUDINARY_API_KEY",
+    "Shutting\x20down\x20server\x20due\x20to\x20uncaught\x20exception",
     "NODE_ENV",
-    "./config/config.env",
-    "unhandledRejection",
+    "798EhiuUm",
+    "ERROR:\x20",
+    "4213512vBHCzb",
     "dotenv",
+    "422358ywIGqp",
+    "Shutting\x20down\x20the\x20server\x20due\x20to\x20Unhandled\x20Promise\x20rejection",
+    "config",
+    "unhandledRejection\x20",
     "./config/database",
-    "CLOUDINARY_API_SECRET",
-    "listen",
+    "246041ZccFty",
     "log",
-    "1055696sPbykZ",
-    "661352OwNKpq",
-    "568547MMPKqX",
-    "369417DJZlqc",
-    "808989nUVCah",
-    "393188lClEkX",
-    "791862TyLYXP",
+    "13006mXNCxl",
+    "env",
+    "./config/config.env",
+    "9djIoyW",
   ];
-  _0x4126 = function () {
-    return _0x3c6e59;
+  _0x10b6 = function () {
+    return _0x5cdbf7;
   };
-  return _0x4126();
+  return _0x10b6();
 }
+
+const PORT = process[_0x948281(0x170)][_0x948281(0x174)] || 4000;
+const server = app[_0x948281(0x17c)](PORT, () => {
+  const _0x9eb2d = _0x948281;
+  console[_0x9eb2d(0x16e)](
+    "Server\x20started\x20on\x20PORT:\x20" +
+      PORT +
+      "\x20in\x20" +
+      process[_0x9eb2d(0x170)][_0x9eb2d(0x181)] +
+      "\x20mode."
+  );
+});
+process["on"](_0x948281(0x16b), (_0x560a3e) => {
+  const _0x518dcc = _0x948281;
+  console[_0x518dcc(0x16e)](_0x518dcc(0x183) + _0x560a3e["message"]),
+    console[_0x518dcc(0x16e)](_0x518dcc(0x187)),
+    server[_0x518dcc(0x17e)](() => {
+      process["exit"](0x1);
+    });
+});
